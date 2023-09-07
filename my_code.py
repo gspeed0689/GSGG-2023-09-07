a931eb6ed1a06b11a18145be391d2cbcda24b5ce
gspeed0689 <6378547+gspeed0689@users.noreply.github.com>
2023-09-07 14:50:08 +0200

    Added documentation to power func

--- my_code.py
+++ my_code.py
@@ -1,5 +1,11 @@
 def power(x, y):
-    rv = x
+    """Exponent function
+
+    Args:
+        x (int): Any positive integer
+        y (int): Any positive integer
+    """
+    rv = x # rv = return value
     for i in range(y):
         rv = rv * x
     return(rv)
